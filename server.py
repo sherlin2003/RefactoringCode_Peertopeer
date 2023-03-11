87cbd3c88a767f585975ab294860a1819ab9ecf0
sherlin2003 <swethasam2014@gmail.com>
2023-03-11 21:58:11 +0530

    reducedcomplexity

--- server.py
+++ server.py
@@ -39,63 +39,67 @@ class server:
         peer_id = uuid.uuid4().int >> 115
         self.peers[peer_id] = addr
         client.send(pickle.dumps((peer_id, addr[1])))
-    
+        
         while True:
             method = pickle.loads(client.recv(max_chunk))
-    
             print("Method opted by peer_id-", peer_id, " is ", method)
-    
+        
             if method == "search":
-                client.send(pickle.dumps("ok"))
-                file_name = pickle.loads(client.recv(max_chunk))
-    
-                if file_name in self.file:
-                    client.send(pickle.dumps('found'))
-                    reply = pickle.loads(client.recv(max_chunk))
-    
-                    if reply == 'send':
-                        content = pickle.dumps(self.file[file_name])
-                        client.send(content)
-    
-                        peerno = pickle.loads(client.recv(max_chunk))
-                        client.send(pickle.dumps(self.peers[peerno]))
-                    else:
-                        print("only search is done..")
-                else:
-                    client.send(pickle.dumps("not found"))
-    
+                self.handle_search(client)
             elif method == 'register':
-                client.send(pickle.dumps('ok'))
-                file_name = pickle.loads(client.recv(max_chunk))
-                print("For registering file by peer_id-", peer_id)
-    
-                if file_name in self.file:
-                    self.file[file_name].append(peer_id)
-                else:
-                    self.file[file_name] = [peer_id]
-    
-                client.send(pickle.dumps('success'))
-    
+                self.handle_register(client, peer_id)
             elif method == 'bye':
-                client.send(pickle.dumps('ok'))
-    
-                list1 = []
-    
-                for i in self.file:
-                    try:
-                        self.file[i].remove(peer_id)
-                        if not self.file[i]:
-                            list1.append(i)
-                    except ValueError:
-                        continue
-    
-                for i in list1:
-                    del self.file[i]
-    
-                del self.peers[peer_id]
-                sys.exit(0)
+                self.handle_bye(client, peer_id)
+                break
 
+    def handle_search(self, client):
+        client.send(pickle.dumps("ok"))
+        file_name = pickle.loads(client.recv(max_chunk))
+        
+        if file_name in self.file:
+            client.send(pickle.dumps('found'))
+            reply = pickle.loads(client.recv(max_chunk))
+        
+            if reply == 'send':
+                content = pickle.dumps(self.file[file_name])
+                client.send(content)
+                peerno = pickle.loads(client.recv(max_chunk))
+                client.send(pickle.dumps(self.peers[peerno]))
+            else:
+                print("only search is done..")
+        else:
+            client.send(pickle.dumps("not found"))
     
+    def handle_register(self, client, peer_id):
+        client.send(pickle.dumps('ok'))
+        file_name = pickle.loads(client.recv(max_chunk))
+        print("For registering file by peer_id-", peer_id)
+        
+        if file_name in self.file:
+            self.file[file_name].append(peer_id)
+        else:
+            self.file[file_name] = [peer_id]
+        
+        client.send(pickle.dumps('success'))
+    
+    def handle_bye(self, client, peer_id):
+        client.send(pickle.dumps('ok'))
+        
+        list1 = []
+        for i in self.file:
+            try:
+                self.file[i].remove(peer_id)
+                if not self.file[i]:
+                    list1.append(i)
+            except ValueError:
+                continue
+        
+        for i in list1:
+            del self.file[i]
+        
+        del self.peers[peer_id]
+    
+        
 
     def connections(self):
         self.sock.listen(self.no_of_connections)
